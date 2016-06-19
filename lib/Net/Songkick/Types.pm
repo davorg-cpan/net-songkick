562a8014cbb419aefe0b7e5a0711da35deea85ff
Dave Cross <dave@dave.org.uk>
2016-06-19 17:27:26 +0100

    Added pod to Net::Songkick::Types.

--- lib/Net/Songkick/Types.pm
+++ lib/Net/Songkick/Types.pm
@@ -1,3 +1,14 @@
+=head1 NAME
+
+Net::Songkick::Types - USeful type stuff for Net::Songkick
+
+=head1 SYNOPSIS
+
+  use Net::Songkick::Types;
+  # This really won't be very useful outside of Net::Songkick.
+
+=cut
+
 package Net::Songkick::Types;
 
 use Moose::Util::TypeConstraints;
@@ -18,3 +29,21 @@ coerce 'Net::Songkick::DateTime',
   };
 
 1;
+
+=head1 AUTHOR
+
+Dave Cross <dave@perlhacks.com>
+
+=head1 SEE ALSO
+
+L<Net::Songkick>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2016, Magnum Solutions Ltd.  All Rights Reserved.
+
+This script is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
